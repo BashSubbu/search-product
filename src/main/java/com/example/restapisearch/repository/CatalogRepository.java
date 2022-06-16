59992c192ecb0c84b6bbc8d62c77fe7309b4aa26
sivasubrahmanyam <sivasubrahmanyam.pagolu@techwave.net>
2022-06-17 00:55:24 +0530

    search catalog

--- src/main/java/com/example/restapisearch/repository/CatalogRepository.java
+++ src/main/java/com/example/restapisearch/repository/CatalogRepository.java
@@ -6,6 +6,7 @@ import com.example.restapisearch.entity.Catalog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -13,4 +14,7 @@ import org.springframework.stereotype.Repository;
 public interface CatalogRepository extends JpaRepository<Catalog,Long>{
     @Query(value = "select c.id as Id,c.name as Name,ca.asset_name as assetName from catalog c INNER JOIN catalog_assets ca on c.id=ca.catalog_id",nativeQuery = true)
     List<CatalogProjection> getAllCatalogsInfo();
+
+    @Query(value = "select DISTINCT c.id as Id,c.name as Name,ca.asset_name as assetName from catalog c INNER JOIN catalog_assets ca on c.id=ca.catalog_id where c.name LIKE CONCAT('%',:searchText, '%') OR ca.asset_name LIKE CONCAT('%',:searchText, '%')",nativeQuery = true)
+    List<CatalogProjection> getAllCatalogSearchInfo(@Param(value = "searchText") String searchText);
 }
