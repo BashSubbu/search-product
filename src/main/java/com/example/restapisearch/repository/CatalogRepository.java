6d9e6b9848710364b932e1dc68015f19b016c56c
sivasubrahmanyam <sivasubrahmanyam.pagolu@techwave.net>
2022-06-18 22:27:30 +0530

    f1

--- src/main/java/com/example/restapisearch/repository/CatalogRepository.java
+++ src/main/java/com/example/restapisearch/repository/CatalogRepository.java
@@ -16,6 +16,7 @@ public interface CatalogRepository extends JpaRepository<Catalog,Long>{
     @Query(value = "select DISTINCT c.id as Id,c.name as Name,ca.asset_name as assetName from catalog c INNER JOIN catalog_assets ca on c.id=ca.catalog_id",nativeQuery = true)
     List<CatalogProjection> getAllCatalogsInfo();
 
+    
     @Query(value = "select DISTINCT c.id as Id,c.name as Name,ca.asset_name as assetName from catalog c INNER JOIN catalog_assets ca on c.id=ca.catalog_id where c.name LIKE CONCAT('%',:searchText, '%') OR ca.asset_name LIKE CONCAT('%',:searchText, '%')",nativeQuery = true)
     List<CatalogProjection> getAllCatalogSearchInfo(@Param(value = "searchText") String searchText);
 }
