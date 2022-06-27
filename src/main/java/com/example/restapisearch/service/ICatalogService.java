36ff796f84b416a78bb6e3f70da896577fa942a1
sivasubrahmanyam <sivasubrahmanyam.pagolu@techwave.net>
2022-06-27 22:03:46 +0530

    some changes2

--- src/main/java/com/example/restapisearch/service/ICatalogService.java
+++ src/main/java/com/example/restapisearch/service/ICatalogService.java
@@ -3,12 +3,9 @@ package com.example.restapisearch.service;
 import java.util.List;
 
 import com.example.restapisearch.dto.CatalogProjection;
-<<<<<<< HEAD
 
 
-=======
 // some feature branch changes
->>>>>>> feature_branch1
 public interface ICatalogService {
     List<CatalogProjection> getAllCatalogsInfo();
     List<CatalogProjection> getAllCatalogSearchInfo(String searchText);
